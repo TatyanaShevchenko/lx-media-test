acb5ca72344ee84dea5a56cbcd51a1faf98eb357
Tatyana Shevchenko <t.shevchenko@mafin.ru>
2021-06-26 07:40:28 +0300

    feat(slider): add slider component

--- components/slider/Slider.jsx
+++ components/slider/Slider.jsx
@@ -1,5 +1,239 @@
+import { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import classnames from 'classnames'
+
+import 'swiper/swiper.min.css'
+
 import styles from './index.module.scss'
 
+import SwiperCore, { Navigation } from 'swiper/core'
+
+// configure Swiper to use modules
+SwiperCore.use([Navigation])
+
+let bg = 'white'
+let image = ''
+
 export const Slider = () => {
-    return <section className={styles.slider}>Career</section>
+    // need setState to re-render component
+    const [activeSlide, setActiveSlide] = useState(0)
+
+    // set slider background and background-image depending on the active slide
+
+    function setBackgroundColor(activeIndex) {
+        switch (activeIndex) {
+            case 0:
+                bg = 'white'
+                image = ''
+                break
+            case 1:
+                bg = '#85868a'
+                image = '/images/slides/slide_2.svg'
+                break
+            case 2:
+                bg = '#ff6600'
+                image = '/images/slides/slide_3.svg'
+                break
+            case 3:
+                bg = '#000000'
+                image = '/images/slides/slide_2.svg'
+                break
+            default:
+                bg = 'white'
+        }
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <div
+                className={styles.back}
+                style={{
+                    backgroundColor: `${bg}`,
+                }}
+            >
+                <div
+                    className={styles.back__image}
+                    style={{
+                        backgroundImage: `url(${image})`,
+                    }}
+                ></div>
+            </div>
+            <Swiper
+                navigation
+                slidesPerView={2}
+                spaceBetween={0}
+                centeredSlides={true}
+                className={styles.slider}
+                onSwiper={(swiper) => console.log('swiper', swiper)}
+                onSlideChange={(index) => setBackgroundColor(index.activeIndex)}
+            >
+                <SwiperSlide className={styles.slide}>
+                    {({ isActive }) => {
+                        if (isActive) {
+                            setActiveSlide(0)
+                        }
+
+                        return (
+                            <div
+                                className={classnames(
+                                    styles.slide__first,
+                                    isActive && styles.slide__first_visible
+                                )}
+                            >
+                                <div className={styles.slide__first__col}>
+                                    <div
+                                        className={classnames(
+                                            styles.slide__first__row,
+                                            styles.slide__first__image
+                                        )}
+                                    ></div>
+                                    <div className={styles.slide__first__row}>
+                                        <p
+                                            className={
+                                                styles.slide__first__title
+                                            }
+                                        >
+                                            Streams
+                                        </p>
+                                        <p
+                                            className={
+                                                styles.slide__first__text
+                                            }
+                                        >
+                                            Antectiur? Sant et qui cum aut
+                                            magnate prae nusdam, que vel modi
+                                            nulpa dolorpost pra velia am,
+                                            incient autem estis qui aut que
+                                            sinienem. Et quae porem apienia
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className={styles.slide__first__col}>
+                                    <p className={styles.slide__first__slogan}>
+                                        Drag to&nbsp;
+                                        <span
+                                            className={
+                                                styles.slide__first__slogan_bold
+                                            }
+                                        >
+                                            discover
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        )
+                    }}
+                </SwiperSlide>
+                <SwiperSlide className={styles.slide}>
+                    {({ isActive }) => {
+                        if (isActive) {
+                            setActiveSlide(1)
+                        }
+                        return (
+                            <div
+                                className={classnames(
+                                    styles.slide__inner,
+                                    isActive && styles.slide__active
+                                )}
+                            >
+                                <div
+                                    className={classnames(
+                                        styles.slide__inner__content,
+                                        styles.slide__inner__content_whitebg
+                                    )}
+                                >
+                                    <p className={styles.slide__description}>
+                                        Success in five phases
+                                    </p>
+                                    <p className={styles.slide__title}>
+                                        Devops
+                                    </p>
+                                    <a
+                                        className={classnames(
+                                            styles.slide__link,
+                                            isActive &&
+                                                styles.slide__link_active
+                                        )}
+                                        href="#"
+                                    >
+                                        Learn more
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    }}
+                </SwiperSlide>
+                <SwiperSlide className={styles.slide}>
+                    {({ isActive }) => {
+                        if (isActive) {
+                            setActiveSlide(2)
+                        }
+                        return (
+                            <div
+                                className={classnames(
+                                    styles.slide__inner,
+                                    isActive && styles.slide__active
+                                )}
+                            >
+                                <div className={styles.slide__inner__content}>
+                                    <p className={styles.slide__description}>
+                                        Making data usable efficiently
+                                    </p>
+                                    <p className={styles.slide__title}>
+                                        Business Intelligence
+                                    </p>
+                                    <a
+                                        className={classnames(
+                                            styles.slide__link,
+                                            isActive &&
+                                                styles.slide__link_active
+                                        )}
+                                        href="#"
+                                    >
+                                        Learn more
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    }}
+                </SwiperSlide>
+                <SwiperSlide className={styles.slide}>
+                    {({ isActive }) => {
+                        if (isActive) {
+                            setActiveSlide(3)
+                        }
+                        return (
+                            <div
+                                className={classnames(
+                                    styles.slide__inner,
+                                    isActive && styles.slide__active
+                                )}
+                            >
+                                <div className={styles.slide__inner__content}>
+                                    <p className={styles.slide__description}>
+                                        Search for new technologies and business
+                                        models worldwide
+                                    </p>
+                                    <p className={styles.slide__title}>
+                                        Scouting
+                                    </p>
+                                    <a
+                                        className={classnames(
+                                            styles.slide__link,
+                                            isActive &&
+                                                styles.slide__link_active
+                                        )}
+                                        href="#"
+                                    >
+                                        Learn more
+                                    </a>
+                                </div>
+                            </div>
+                        )
+                    }}
+                </SwiperSlide>
+            </Swiper>
+        </div>
+    )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,16 @@ import Head from 'next/head'
 import { Banner } from '../components/banner'
 import { AboutUs } from '../components/about-us'
 import { Career } from '../components/career'
+import { Slider } from '../components/slider'
 import { Footer } from '../components/footer'
 
 export default function Home() {
+    const images = [
+        'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+        'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+        'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+        'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80',
+    ]
     return (
         <div>
             <Head>
@@ -29,6 +36,7 @@ export default function Home() {
                 <Banner />
                 <AboutUs />
                 <Career />
+                <Slider images={images} />
                 <Footer />
             </div>
         </div>
